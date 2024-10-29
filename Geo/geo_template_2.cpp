f67042db0a23e0e3539b628f3cd7d0599c553e5b
Mainul Hasan <62393480+mainul3195@users.noreply.github.com>
2024-10-30 00:32:36 +0600

    kjkh

--- Geo/geo_template_2.cpp
+++ Geo/geo_template_2.cpp
@@ -509,39 +509,11 @@ events.push_back({st - eps, +1}); // take care of precisions!
 events.push_back({ed, -1});if (st > ed){events.push_back({-PI, +1});events.push_back({+PI, -1});}}sort(events.begin(), events.end());int cnt = 0;for (auto &&e : events){cnt += e.second;if (cnt > ans){ans = cnt;id = i;th = e.first;}}}PT w = PT(p[id].x + r * cos(th), p[id].y + r * sin(th));c = circle(w, r); // best_circle
 return ans;}
 // radius of the maximum inscribed circle in a convex polygon
-double maximum_inscribed_circle(vector<PT> p)
-{
-    int n = p.size();
-    if (n <= 2)
-        return 0;
-    double l = 0, r = 20000;
-    while (r - l > eps)
-    {
-        double mid = (l + r) * 0.5;
-        vector<HP> h;
-        const int L = 1e9;
-        h.push_back(HP(PT(-L, -L), PT(L, -L)));
-        h.push_back(HP(PT(L, -L), PT(L, L)));
-        h.push_back(HP(PT(L, L), PT(-L, L)));
-        h.push_back(HP(PT(-L, L), PT(-L, -L)));
-        for (int i = 0; i < n; i++)
-        {
-            PT z = (p[(i + 1) % n] - p[i]).perp();
-            z = z.truncate(mid);
-            PT y = p[i] + z, q = p[(i + 1) % n] + z;
-            h.push_back(HP(p[i] + z, p[(i + 1) % n] + z));
-        }
-        vector<PT> nw = half_plane_intersection(h);
-        if (!nw.empty())
-            l = mid;
-        else
-            r = mid;
-    }
-    return l;
-}
+double maximum_inscribed_circle(vector<PT> p){int n = p.size();if (n <= 2)return 0;double l = 0, r = 20000;
+while (r - l > eps){double mid = (l + r) * 0.5;vector<HP> h;const int L = 1e9;h.push_back(HP(PT(-L, -L), PT(L, -L)));h.push_back(HP(PT(L, -L), PT(L, L)));h.push_back(HP(PT(L, L), PT(-L, L)));h.push_back(HP(PT(-L, L), PT(-L, -L)));for (int i = 0; i < n; i++){PT z = (p[(i + 1) % n] - p[i]).perp();z = z.truncate(mid);PT y = p[i] + z, q = p[(i + 1) % n] + z;h.push_back(HP(p[i] + z, p[(i + 1) % n] + z));}vector<PT> nw = half_plane_intersection(h);if (!nw.empty())l = mid;elser = mid;}
+return l;}
 // ear decomposition, O(n^3) but faster
-vector<vector<PT>> triangulate(vector<PT> p)
-{
+vector<vector<PT>> triangulate(vector<PT> p){
     vector<vector<PT>> v;
     while (p.size() >= 3)
     {
